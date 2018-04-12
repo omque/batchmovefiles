dc15f24d852dab504e242f97d45ad71782308b5d
omque <omqazi@gmail.com>
2018-04-12 11:39:35 +0900

    Redefined to allow for user input paths and file extensions.

--- batchmovefiles.py
+++ batchmovefiles.py
@@ -3,13 +3,13 @@
 
 import os, shutil
 
-source_path = os.walk("/Users/Omair/MKVInput")
-dest_path = "/Users/Omair/MKVOutput"
-
+source_path = os.walk(input("What is the source path?" + "\n"))
+dest_path = input("What is the destination path?" + "\n")
+file_extension = input("What is the file extension?" + "\n")
 
 for folderName, subfolders, filename in source_path:
 	for name in filename:
-		if name.endswith(".mkv"):
+		if name.endswith(file_extension):
 			print("File found: " + name)
 			#Define source folder name and join with file name. Send to Destination Path.
 			shutil.move(os.path.join(folderName, name), dest_path)
